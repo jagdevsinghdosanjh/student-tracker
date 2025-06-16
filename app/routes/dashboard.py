d849c48eb2d0093605957859b08fe8df26a2c43b
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-06-17 02:01:42 +0530

    Ok

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -2,6 +2,6 @@ from flask import render_template, Blueprint
 
 score_bp = Blueprint('score', __name__)
 
-@score_bp.route('/dashboard/')
+@score_bp.route('dashboard')
 def dashboard():
     return render_template('index.html')
