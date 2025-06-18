3c6880fd2e0735ba53a430a3735ac03b5e2a89ab
jagdevsinghdosanjh <jagdevsinghdosanjh@gmail.com>
2025-06-18 18:01:34 +0530

    Ok

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -3,11 +3,13 @@
 # In dashboard.py
 from flask import Blueprint, render_template
 
-dashboard_bp = Blueprint('dashboard', __name__)
+dashboard_bp = Blueprint("dashboard", __name__)
 
-@dashboard_bp.route('/dashboard')
+
+@dashboard_bp.route("/dashboard")
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template("dashboard.html")
+
 
 # from flask import render_template, Blueprint
 
